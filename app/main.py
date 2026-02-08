341ae74475767ab4f96faf9330cf993bde27989f
Željana Vajagić <zeljanabovan@gmail.com>
2026-02-08 23:36:51 +0100

    resolved

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class : int, clean_mark : int, brand : str):
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,10 +8,10 @@ class Car:
 class CarWashStation:
     def __init__(
         self,
-        distance_from_city_center,
-        clean_power,
-        average_rating,
-        count_of_ratings,
+        distance_from_city_center : float,
+        clean_power : int,
+        average_rating :float,
+        count_of_ratings : int,
     ):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -28,7 +28,7 @@ class CarWashStation:
 
         return round(income, 1)
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car : Car):
         washing_price = (
             car.comfort_class
             * (self.clean_power - car.clean_mark)
@@ -37,14 +37,14 @@ class CarWashStation:
         )
         return round(washing_price, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car : Car):
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rate):
+    def rate_service(self, rate : float):
         self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + rate) /
-            (self.count_of_ratings + 1),
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1),
             1
         )
         self.count_of_ratings += 1
